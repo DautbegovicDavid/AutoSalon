09b2b988b401e71374e5614f3266862a77b70fed
David Dautbegović <david.dautbegovic@edu.fit.ba>
2018-11-12 23:44:31 +0100

    Pokusaj.1-12.11.2018

---
+++ AutoSalon/AutoSalon/EF/myContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoSalon.EF
+{
+    public class myContext:DbContext
+    {
+
+    }
+}
