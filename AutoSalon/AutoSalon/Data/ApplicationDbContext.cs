cbfcdfb5d1a326923eb37491db57ca660db86150
edinsmajlagic <34797381+edinsmajlagic@users.noreply.github.com>
2018-11-13 23:38:40 +0100

    Kreiranje web aplikacije sa uključenim Microsoft.AspNetCore.Identity-om za autentifikaciju i autorizaciju
    Related Work Items: #8

---
+++ AutoSalon/AutoSalon/Data/ApplicationDbContext.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AutoSalon.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AutoSalon.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser,IdentityRole<int>,int>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable(name: "User");
+                entity.Property(e => e.Id).HasColumnName("UserID");
+
+            });
+
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Role");
+                entity.Property(e => e.Id).HasColumnName("RoleID");
+
+            });
+
+            builder.Entity<IdentityUserClaim<int>>(entity =>
+            {
+                entity.ToTable("UserClaim");
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+                entity.Property(e => e.Id).HasColumnName("UserClaimID");
+
+            });
+
+            builder.Entity<IdentityUserLogin<int>>(entity =>
+            {
+                entity.ToTable("UserLogin");
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            });
+
+            builder.Entity<IdentityRoleClaim<int>>(entity =>
+            {
+                entity.ToTable("RoleClaim");
+                entity.Property(e => e.Id).HasColumnName("RoleClaimID");
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+            });
+
+            builder.Entity<IdentityUserRole<int>>(entity =>
+            {
+                entity.ToTable("UserRole");
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+            });
+
+
+            builder.Entity<IdentityUserToken<int>>(entity =>
+            {
+                entity.ToTable("UserToken");
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            });
+
+        }
+    }
+}
