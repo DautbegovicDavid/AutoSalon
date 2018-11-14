4a1a092d8156ceb896afdcb1bb9eb59852f7d078
edinsmajlagic <34797381+edinsmajlagic@users.noreply.github.com>
2018-11-14 15:41:48 +0100

    Validacija poslovnica sa akcijama Index i Dodaj

--- AutoSalon/AutoSalon/Controllers/PoslovnicaController.cs
+++ AutoSalon/AutoSalon/Controllers/PoslovnicaController.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoSalon.Data;
+using AutoSalon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoSalon.Controllers
 {
     public class PoslovnicaController : Controller
     {
-        public IActionResult Index()
+        private ApplicationDbContext _context;
+
+        public PoslovnicaController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var poslovnice = _context.Poslovnice.Include(p => p.Grad).ToListAsync();
+            return View(await poslovnice);
+        }
+
+        //Get Dodaj
+        public IActionResult Dodaj()
+        {
+            ViewData["GradID"] = new SelectList(_context.Gradovi, "GradID", "Naziv");
             return View();
+        }
+
+        //Post Dodaj
+        [HttpPost]
+        public async Task<IActionResult> Dodaj([Bind("PoslovnicaID,GradID,Adresa,Naziv,KontaktTelefon")]Poslovnica poslovnica)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Poslovnice.Add(poslovnica);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["GradID"] = new SelectList(_context.Gradovi, "GradID", "Naziv", poslovnica.GradID);
+            return View(poslovnica);
         }
     }
 }
