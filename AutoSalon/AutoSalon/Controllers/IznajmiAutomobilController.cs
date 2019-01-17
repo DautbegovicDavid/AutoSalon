6a3a8827811ee42c9dda78c76c253e6ace6647c7
David Dautbegović <david.dautbegovic@edu.fit.ba>
2019-01-17 01:07:33 +0100

    Iznajmljivanje
    Related Work Items: #22, #26

---
+++ AutoSalon/AutoSalon/Controllers/IznajmiAutomobilController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Autosalon.Models;
+using AutoSalon.Models.ViewModels.AutomobilViewModels;
+using AutoSalon.Models;
+using AutoSalon.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoSalon.Controllers
+{
+    public class IznajmiAutomobilController : Controller
+    {
+        public ApplicationDbContext db;
+        public IznajmiAutomobilController (ApplicationDbContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            AutomobilRent model = new AutomobilRent();
+
+            model.auta = db.Automobil.Select(x => new AutomobilRent.Auto
+            {
+                
+                Boja = x.Boja,
+                Model = x.Model,
+                Proizvodjac = x.Proizvodjac.Naziv,
+                GodinaProizvodnje = x.GodinaProizvodnje,
+                SlikaURL = x.SlikaURL,
+                Gorivo = x.AutomobilDetalji.Gorivo,
+                Transmisija = x.AutomobilDetalji.Transmisija,
+                
+                Kilovati = x.AutomobilDetalji.Kilovati,
+                BrojSjedista = x.AutomobilDetalji.BrojSjedista,
+                BrojVrata = x.AutomobilDetalji.BrojVrata,
+                CijenaRentanja = x.AutomobilDetalji.CijenaRentanja,
+                
+                Pogon=x.AutomobilDetalji.Pogon,
+                Kubikaza=x.AutomobilDetalji.Kubikaza,
+                EmisioniStandard=x.AutomobilDetalji.EmisioniStandard
+
+
+
+            }).ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ AutoSalon/AutoSalon/Models/ViewModels/AutomobilViewModels/AutomobilRent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoSalon.Models.ViewModels.AutomobilViewModels
+{
+    public class AutomobilRent
+    {
+        public List<Auto> auta { get; set; }
+        public class Auto
+        {
+            public int AutomobilID { get; set; }
+            public int AutomobilDetaljiID { get; set; }
+
+            public string Model { get; set; }
+            public string Proizvodjac { get; set; }
+            public int GodinaProizvodnje { get; set; }
+            public string Boja { get; set; }
+            public bool Novo { get; set; }
+            public string SlikaURL { get; set; }
+            
+            public string Gorivo { get; set; }
+            public string Transmisija { get; set; }
+            public string Pogon { get; set; }
+            public int Kilovati { get; set; }
+            
+            
+           
+            public string BrojSjedista { get; set; }
+            public string BrojVrata { get; set; }
+            public string Kubikaza { get; set; }
+            public string Tip { get; set; }
+            public string EmisioniStandard { get; set; }
+            
+            public decimal? CijenaRentanja { get; set; }
+        }
+    }
+}
